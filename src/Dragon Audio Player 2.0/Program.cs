dc36c8cf9c98573d4d5d6c59cb17b42403f70f99
Lorenzo <sheepytaggernep@gmail.com>
2014-11-26 14:00:51 +0100

    Revert "Deltee all"
    This reverts commit 0d3f06222af918c38b155da9d6e6cecc944b1ff7.

---
+++ src/Dragon Audio Player 2.0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Dragon_Audio_Player
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
