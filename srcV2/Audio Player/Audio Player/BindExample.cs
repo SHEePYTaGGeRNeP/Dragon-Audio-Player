ccd3331121fc63dac10aeb999ae14bb6be8a8848
SHEePYTaGGeRNeP <neppy@live.nl>
2017-02-24 19:37:49 +0100

    Added data binding to new project

---
+++ srcV2/Audio Player/Audio Player/BindExample.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Audio_Player
+{
+    class BindExample
+    {
+        public List<BindClass> Bind = new List<BindClass>();
+    }
+
+    public class BindClass : INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                this.OnPropertyChanged("Name");
+            }
+        }
+
+        private string name2;
+        public string Name2
+        {
+            get { return name2; }
+            set
+            {
+                name2 = value;
+                this.OnPropertyChanged("Name2");
+            }
+        }
+
+        private DateTime modifiedDate;
+
+        public DateTime ModifiedDate
+        {
+            get { return modifiedDate; }
+            set { modifiedDate = value; }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- srcV2/Audio Player/Audio Player/MainWindow.xaml.cs
+++ srcV2/Audio Player/Audio Player/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,43 @@ namespace Audio_Player
     /// </summary>
     public partial class MainWindow : Window
     {
+        Thread itemThread;
+        private BindExample _be = new BindExample();
+
         public MainWindow()
         {
             InitializeComponent();
+            this.lvData.ItemsSource = this._be.Bind;
+            this.Initialise();
+        }
+
+        private void Initialise()
+        {
+            itemThread = new Thread(() =>
+            {
+                int i = 0;
+                while (true)
+                {
+                    i++;
+                    this._be.Bind.Add(new BindClass() { Name = "Hello", Name2 = " test", ModifiedDate = DateTime.Now });
+                    Dispatcher.BeginInvoke((Action)(() =>
+                    {
+                        this.lvData.Items.Refresh();
+                    }));
+                    Thread.Sleep(500);
+                    if (i > 5)
+                    {
+                        i = 0;
+                        this._be.Bind.Clear();
+                    }
+                }
+            });
+            itemThread.Start();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            itemThread.Abort();
         }
     }
 }
